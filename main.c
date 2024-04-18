0024dbd05155104fc4366679aef98eeccc4f4e3e
MaximeCln <82881194+MaximeCln@users.noreply.github.com>
2024-04-18 14:55:05 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,130 @@
-#include "main.h"
-#include "column.h"
-#include "cdataframe.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-int main(){
-    COLUMN *mycol = create_column("My column");
-    insert_value(mycol, 52);
-    insert_value(mycol, 44);
-    insert_value(mycol, 15);
-    COLUMN *mycol2= create_column("My column2");
-    insert_value(mycol2, 1);
-    insert_value(mycol2, 2);
-    insert_value(mycol2, 3);
-    CDATAFRAME *mycdf = create_cdataframe(5);
-    add_columns(mycdf, mycol);
-    add_columns(mycdf, mycol2);
-    print_cdf(mycdf);
+#define REALOC_SIZE 256
+// need to comment the code
+typedef struct {
+    char *title;
+    int physical_size;
+    int logical_size;
+    int *data;
+} COLUMN;
+
+COLUMN *create_col(char *title) {
+    COLUMN *col = (COLUMN *)malloc(sizeof(COLUMN));
+    if (col) {
+        col->title = title;
+        col->data = NULL;
+        col->physical_size = 0;
+        col->logical_size = 0;
+    }
+    return col;
+}
+
+int insert_val(COLUMN *col, int value) {
+    if (!col)
+        return 0;
+
+    if (col->logical_size == col->physical_size) {
+        int *temp = realloc(col->data, (col->physical_size + REALOC_SIZE) * sizeof(int));
+        if (!temp)
+            return 0;
+        col->data = temp;
+        col->physical_size += REALOC_SIZE;
+    }
+
+    col->data[col->logical_size++] = value;
+    return 1;
+}
+
+void delete_col(COLUMN **col) {
+    if (!col || !(*col))
+        return;
+
+    free((*col)->data);
+    free((*col)->title);
+    free(*col);
+    *col = NULL;
+}
+
+void print_col(COLUMN *col) {
+    if (!col)
+        return;
+
+    printf("[%s]\n", col->title);
+    for (int i = 0; i < col->logical_size; i++) {
+        printf("[%d] %d\n", i, col->data[i]);
+    }
+}
+
+int count_occ(COLUMN *col, int x) {
+    if (!col)
+        return 0;
+
+    int count = 0;
+    for (int i = 0; i < col->logical_size; i++) {
+        if (col->data[i] == x)
+            count++;
+    }
+    return count;
+}
+
+int positionval(COLUMN *col, int x) {
+    if (!col || x < 0 || x >= col->logical_size)
+        return -1; // meaning a error
+
+    return col->data[x];
+}
+
+int valuegreater(COLUMN *col, int x) {
+    if (!col)
+        return 0;
+
+    int count = 0;
+    for (int i = 0; i < col->logical_size; i++) {
+        if (col->data[i] > x)
+            count++;
+    }
+    return count;
+}
+
+int valueless(COLUMN *col, int x) {
+    if (!col)
+        return 0;
+
+    int count = 0;
+    for (int i = 0; i < col->logical_size; i++) {
+        if (col->data[i] < x)
+            count++;
+    }
+    return count;
+}
+
+int valuequal(COLUMN *col, int x) {
+    if (!col)
+        return 0;
+
+    int count = 0;
+    for (int i = 0; i < col->logical_size; i++) {
+        if (col->data[i] == x)
+            count++;
+    }
+    return count;
+}
+
+int main() {
+    COLUMN *mycol = create_col("My column");
+    insert_val(mycol, 52);
+    insert_val(mycol, 44);
+    insert_val(mycol, 15);
+    print_col(mycol);
+
+    printf("Occurrences of 44: %d\n", count_occ(mycol, 44));
+    printf("Value at position 1: %d\n", positionval(mycol, 1));
+    printf("Values greater than 20: %d\n", valuegreater(mycol, 20));
+    printf("Values less than 30: %d\n", valueless(mycol, 30));
+    printf("Values equal to 44: %d\n", valuequal(mycol, 44));
+
+    delete_col(&mycol);
+    return 0;
 }
