a45b34f222e62e8be135c04b34a7c1cad8ee6874
alban <akestwitch@gmail.com>
2024-04-20 23:51:27 +0200

    Add few prototypes

--- cdataframe.h
+++ cdataframe.h
@@ -14,7 +14,11 @@ CDATAFRAME *create_cdataframe(int size);
 
 int add_column_cdf(CDATAFRAME *cdf, COLUMN *col);
 
+void delete_column_cdf(CDATAFRAME *cdf, char *col_name);
+
 void delete_cdf(CDATAFRAME **cdf);
+
+int get_cdataframe_cols_size(CDATAFRAME *cdf);
 
 void print_cdf(CDATAFRAME * cdf);
 
