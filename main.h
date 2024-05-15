d133c434b13a34c412db6dd2c8712446a1344c82
MaximeCln <maxcolin92@gmail.com>
2024-05-15 14:11:16 +0200

    Menu changes and avancement

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef CDATAFRAME_MAIN_H
+#define CDATAFRAME_MAIN_H
+
+void MenuSection();
+void MenuFilling();
+void MenuDisplaying();
+void MenuUsualOperations();
+void MenuAnalysisStatistics();
+
+#endif //CDATAFRAME_MAIN_H
